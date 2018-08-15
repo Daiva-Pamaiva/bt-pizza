52edb95c45272cfbce19710bab1c8f36306b31ed
JustasKa <41920177+JustasKa@users.noreply.github.com>
2018-08-15 20:22:31 +0300

    Update Pizza.java

--- src/lt/baltictalents/pizzeria/Pizza.java
+++ src/lt/baltictalents/pizzeria/Pizza.java
@@ -1,1 +1,24 @@
 
+String name;
+
+	public Pizza(String name) 
+	{
+	
+		this.name= name;
+	
+	}
+	
+	public String getName() 
+	{
+		
+		return name;
+    
+	}
+	public String toString() {
+	        
+		return name;
+	    
+	}
+	
+}
+	
